6ed73fdca066ca7ee94bb97db8ed4d26bb30d4c8
James Robins <jamesrobins101@hotmail.com>
2018-09-20 09:34:53 +0100

    set up for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,4 @@
 const port = process.env.PORT || 4000;
-const dbURI = 'mongodb://localhost:27017/blog-articles';
+const dbURI = process.env.MONDGODB_URI || 'mongodb://localhost:27017/blog-articles';
+
 module.exports = { port, dbURI };
